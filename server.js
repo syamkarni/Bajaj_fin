3b14a82f6c7550a0a1ca4f960c7bab9727bacecf
Syamkarni Uppalapati <101443460+syamkarni@users.noreply.github.com>
2024-09-22 17:14:02 +0530

    restored the backend files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const mime = require('mime-types');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
